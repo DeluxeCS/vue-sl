cc960fa176068938643d0b7ee2f5fa92eeab464f
DeluxeCS <943388027@qq.com>
2020-08-19 18:33:02 +0800

    通过对el中节点进行遍历获取，更新属性值，再挂载到画面上

--- src/js/CVue.js
+++ src/js/CVue.js
@@ -12,27 +12,34 @@ class CVue {
         this.observe(this.$data);
 
         // 模拟watcher
-        new Watcher();
-        this.$data.test;
-        new Watcher();
-        this.$data.foo.batchch;
+        // new Watcher();
+        // 当读取data中的test,会触发get方法
+        // this.$data.test;
+
+        new Compile(options.el, this);
+
+        if (options.created) {
+            options.created.call(this);
+        }
     }
 
     observe(val) {
         // 确定val存在性、是否为object
-        if (!val || typeof val !== 'object') {
+        if (!val || typeof val !== "object") {
             return;
         }
         // 遍历该对象
-        Object.keys(val).forEach(key => {
+        Object.keys(val).forEach((key) => {
             this.defineReactive(val, key, val[key]);
+            // 添加代理data中的属性放到vue上
+            this.proxyDate(key);
         });
     }
     // 数据响应化函数
     defineReactive(obj, key, val) {
         // 递归数据嵌套
         this.observe(val);
-
+        // console.log(`${obj},${key}`)
         // 初始化dep
         const dep = new Dep();
         Object.defineProperty(obj, key, {
@@ -49,6 +56,18 @@ class CVue {
                 // console.log(`${key}发生变化,更新${val}`)
                 // 通知watcher进行更新
                 dep.notify();
+            },
+        });
+    }
+
+    // 添加代理data中的属性放到vue上
+    proxyDate(key) {
+        Object.defineProperty(this, key, {
+            get: function () {
+                return this.$data[key];
+            },
+            set: function (newVal) {
+                this.$data[key] = newVal;
             }
         })
     }
@@ -66,22 +85,29 @@ class Dep {
 
     //通知依赖
     notify() {
-        this.deps.forEach(dep => {
+        this.deps.forEach((dep) => {
             dep.update();
-        })
+        });
     }
-
 }
 
 // 实现更新依赖
 class Watcher {
-    constructor() {
+    constructor(vm, key, callback) {
+        this.vm = vm;
+        this.key = key;
+        this.callback = callback;
+
         // 将当前watcher实例指定到Dep的静态属性target
         // 当新的watch产生，即覆盖掉
         Dep.target = this;
+        // 触发get方法,向dep添加依赖
+        this.vm[this.key];
+        // 依赖制空
+        Dep.target = null;
     }
 
     update() {
-        console.log('属性更新成功！');
+        this.callback.call(this.vm, this.vm[this.key])
     }
 }
--- src/js/compile.js
+++ src/js/compile.js
@@ -1,8 +1,82 @@
-// new compile(el,vm)
-class compile {
+// new compile(el,vm) 分析元素  获取vue实例
+class Compile {
     constructor(el, vm) {
         // 遍历的宿主节点
         this.$el = document.querySelector(el);
         this.$vm = vm;
+        // 编译
+        if (this.$el) {
+            // 减少DOM操作，提取片段
+            this.$fragment = this.node2Fragment(this.$el);
+            // 执行编译
+            this.compile(this.$fragment);
+            // 编译后，替换后的html结果追加至$el中
+            this.$el.appendChild(this.$fragment);
+        }
+    };
+
+    //  提取片段操作
+    node2Fragment(el) {
+        // 创建一个新的空白的文档片段
+        const frag = document.createDocumentFragment();
+        // 将el子元素移动至frag文档片段中;el循环减少
+        let child;
+        while (el.firstChild) {
+            child = el.firstChild;
+            frag.appendChild(child);
+        }
+        return frag;
+    };
+
+    // 进行编译，为画面属性赋值
+    compile(fragment) {
+        const childNodes = fragment.childNodes;
+        Array.from(childNodes).forEach(node => {
+            // 判断node类型
+            // 文本类型
+            if (this.isInterpolation(node)) {
+                // console.log(`编译文本${node.nodeName}`)
+                // 编译插值文本
+                this.commpileText(node)
+            }
+
+            // 元素类型
+            else if (this.isElement(node)) {
+                // console.log(`编译元素${node.nodeName}`)
+            }
+
+            //  递归子节点
+            if (node.childNodes && node.childNodes.length > 0) {
+                this.compile(node)
+            }
+        })
+    };
+
+    isInterpolation(node) {
+        return node.nodeType === 3 && /\{\{(.*)\}\}/.test(node.textContent);
+    }
+    isElement(node) {
+        return node.nodeType === 1;
+    }
+
+    // 编译插值文本的更新
+    commpileText(node) {
+        this.update(node, this.$vm, RegExp.$1, 'text')
+    }
+
+    // 更新指令
+    // node : 宿主节点。vm,vue实例。 exp表达式。指令：text、html
+    update(node, vm, exp, dir) {
+        const updaterFun = this[dir + 'Updater'];
+        // 初始化
+        updaterFun && updaterFun(node, vm[exp]);
+        // 依赖收集
+        new Watcher(vm, exp, function (newVal) {
+            updaterFun && updaterFun(node, newVal)
+        });
+    }
+
+    textUpdater(node, value) {
+        node.textContent = value;
     }
 }
